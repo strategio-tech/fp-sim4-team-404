f3198983aecf5a1cffff7318efaa15935108843f
srmacam <saremacam@gmail.com>
2022-10-05 14:24:10 -0700

    Added .env variables

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDNau-5ATOhmwoHreFQ1K59_tJgl_Fwm7o",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "cryptek-team-404.firebaseapp.com",
     projectId: "cryptek-team-404",
     storageBucket: "cryptek-team-404.appspot.com",
